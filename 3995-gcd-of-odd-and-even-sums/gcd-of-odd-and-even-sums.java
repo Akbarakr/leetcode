63f6af0bcba8deceef47e94745b110f3f2121291
MOHAMED AKBAR A <akbarakr123@gmail.com>
2026-01-17 11:17:03 +0530

    Time: 1 ms (80.20%) | Memory: 42.5 MB (79.05%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,19 +1,5 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-    
-        int oddsum=0;
-        int evensum=0;
-        for(int i=1;i<=n*2;i=i+2){
-            oddsum=oddsum+i;
-        }
-        
-        for(int i=2;i<=n*2;i=i+2){
-            evensum=evensum+i;
-        }
-        for(int i=Math.min(oddsum,evensum);i>=2;i--){
-            if(oddsum%i==0&&evensum%i==0){
-                return i;
-            }
-        }return 1;
-    }
+        return n;
+}
 }
