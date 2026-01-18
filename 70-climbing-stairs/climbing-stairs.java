613c545bf602e23947eb9b9ab9c2654018a3954a
MOHAMED AKBAR A <akbarakr123@gmail.com>
2026-01-18 20:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (77.74%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,9 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==0){
-                return 1;
-            }else if(n==1){
-                return 1;
+        if(n<=2){
+            return n;
             }
         int sum=0;
         int prev1=1;
