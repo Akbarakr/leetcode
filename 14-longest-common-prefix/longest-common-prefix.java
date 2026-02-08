0c92a1bf88a6734c9efadd22efdfde660ebef132
MOHAMED AKBAR A <akbarakr123@gmail.com>
2026-02-08 22:21:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (29.29%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String p=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(p)!=0){
+                p=p.substring(0, p.length()-1);
+                if(p.isEmpty())
+                return "";
+            }
+        }
+        return p;
+    }
+}
