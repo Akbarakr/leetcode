8a58151b8793bacf42b1ebd2fe1c48cfc01be5a5
MOHAMED AKBAR A <akbarakr123@gmail.com>
2026-02-24 22:58:12 +0530

    Time: 1 ms (99.73%) | Memory: 94.7 MB (30.61%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,13 +2,11 @@ class Solution {
     public int maxProfit(int[] prices) {
         int minprofit=Integer.MAX_VALUE; 
 	    int maxprofit = 0;
-        int profit=0;
         for(int i: prices){
             if(i<minprofit){
                 minprofit=i;
-            }else{
-                profit= i-minprofit;
             }
+            int profit= i-minprofit;
 
             if(profit>maxprofit){
                 maxprofit=profit;
